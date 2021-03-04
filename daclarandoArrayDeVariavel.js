906e2faa315d60923584b75f3c5b349e09b72466
JessicaLigia <47614058+jessicalisc@users.noreply.github.com>
2021-03-03 21:19:54 -0300

    Create daclarandoArrayDeVariavel.js

---
+++ daclarandoArrayDeVariavel.js
@@ -0,0 +1,12 @@
+var doces = [
+  "Doce de Leite"
+  "Doce de Abobora"
+  "Brigadeiro"
+  "Beijinho"
+  "Goiabada"
+  "Chocolate"
+];
+
+console.log("Total de tipos de doces: " + doces.length);
+console.log("Eu gosto de :" + doces[0]);
+console.log("Amanda gosta de: " + doces[2]);
